181bd5c0f71446476613438f214cb6cb9445cd1d
Kerwin-Jin <1351207863@qq.com>
2022-01-17 20:25:02 +0800

    complete

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     // publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/'
     // publicPath: ''   or   publicPath:"./"
-    publicPath: '/'
+    publicPath:"./"
+    // publicPath: '/'
 }
